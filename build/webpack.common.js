a8f4ad9f90dbc8e7a928d9e916de2f675991e4b9
Allen Bai <957820957@qq.com>
2019-03-29 22:41:18 +0800

    add eslint config ; add prettier config ; add eslint-loader enforce lint before compile ; add husky and enforce lint before commit ;

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,19 +1,24 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   resolve: {
-    extensions: [".js", ".json", ".jsx"]
+    extensions: ['.js', '.json', '.jsx']
   },
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        use: 'eslint-loader'
+      },
+      {
         test: /\.jsx$/,
-        use: "babel-loader"
+        use: 'babel-loader'
       },
       {
         test: /\.js$/,
-        use: "babel-loader",
-        exclude: [path.resolve(__dirname, "../node_modules/")]
+        use: 'babel-loader',
+        exclude: [path.resolve(__dirname, '../node_modules/')]
       }
     ]
   }
