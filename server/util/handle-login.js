7da240d055bcfcacf274b75d63f4ce7031b1ea80
Allen Bai <957820957@qq.com>
2019-03-31 23:52:52 +0800

    add login and proxy api

---
+++ server/util/handle-login.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+const baseUrl = "https://cnodejs.org/api/v1";
+
+router.post("/login", function(req, res, next) {
+  console.log("登陆信息");
+  console.log(`${baseUrl}/accesstoken`);
+  console.log(req.body.accessToken);
+  axios
+    .post(`${baseUrl}/accesstoken`, {
+      accesstoken: req.body.accessToken
+    })
+    .then(resp => {
+      if (resp.status === 200 && resp.data.success) {
+        let { loginname, id, avatar_url: avatarUrl } = resp.data;
+        req.session.user = {
+          accessToken: req.body.accessToken,
+          loginName: loginname,
+          id: id,
+          avatarUrl: avatarUrl
+        };
+        res.send({
+          sucess: true,
+          data: req.session.user
+        });
+      }
+    })
+    .catch(err => {
+      if (err.response) {
+        res.json({
+          success: false,
+          data: err.response.data
+        });
+      } else {
+        next(err);
+      }
+    });
+});
+
+module.exports = router;
---
+++ server/util/proxy.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+
+const baseUrl = "https://cnodejs.org/api/v1";
+
+module.exports = function(req, res, next) {
+  const { path } = req;
+  const { user = {} } = req.session;
+  const needAccessToken = req.query.needAccessToken;
+
+  if (needAccessToken && !user.accessToken) {
+    return res.status(401).json({ success: false, msg: "need login" });
+  }
+
+  console.log("代理请求信息");
+  console.log(req.method);
+  console.log(`${baseUrl}${path}`);
+  console.log(req.query);
+  console.log({ ...req.body, accesstoken: user.accessToken });
+
+  axios({
+    method: req.method,
+    url: `${baseUrl}${path}`,
+    params: { ...req.query, accesstoken: req.session.user.accessToken },
+    data: { ...req.body, accesstoken: req.session.user.accessToken }
+    // headers: {
+    //   "Content-Type": "application/x-www-form-urlencoded"
+    // }
+  })
+    .then(resp => {
+      if (resp.status === 200) {
+        res.json(resp.data);
+      } else {
+        res.status(resp.status).send(resp.data);
+      }
+    })
+    .catch(err => {
+      if (err.response) {
+        res.status(500).send(err.response.data);
+      } else {
+        console.error(err);
+        res.status(500).send({
+          success: false,
+          msg: "未知错误"
+        });
+      }
+    });
+};
